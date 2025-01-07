93ede5488069b62c908b29a55b1abf8246ac6125
Patrice976 <catinatp@gmail.com>
2025-01-07 11:09:41 +0100

    import mis à jour coil3 au lieu de coil

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -35,7 +35,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import coil.compose.rememberAsyncImagePainter
+import coil3.compose.rememberAsyncImagePainter
 
 
 //LISTE DES CLASSES
